1325fdcb4433f9a58709c70024c732f19e20eab5
CyrilleMarc <cyrillemarc1@gmail.com>
2023-02-16 14:51:44 +0100

    add footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,17 @@
 import React from "react";
 import NavBar from "../components/Navbar";
+import Footer from '../components/Footer'
 import '../assets/styles/HomePage.css';
 
 function HomePage() {
   return (
-    <div>
+    <div className='HomePage'>
       <NavBar />
       <div className='HomePageButton'>
         <button>Vous êtes recruteur</button>
         <button>Vous êtes développeur</button>
       </div>
+      <Footer />
     </div>
   );
 }
